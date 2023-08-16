dbfbf258007864694da519b02f8372ba464b99b9
Benjamin Filiatrault <benjamin.filiatrault@hotmail.com>
2023-08-16 13:53:09 -0400

    Jenkins is Groovy

--- init.lua
+++ init.lua
@@ -1,1 +1,10 @@
 require("benfiliatro")
+
+-- [[ Filetype Detection ]]
+-- Detect Jenkinsfile as a Groovy Filetype
+vim.filetype.add {
+    filename = {
+        ['Jenkinsfile'] = 'groovy',
+        ['jenkinsfile'] = 'groovy',
+    },
+}
