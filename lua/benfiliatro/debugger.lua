a1685f1270f29e6d213423332b28a6feeeacc72b
Benjamin Filiatrault <37634960+benjaminfiliatrault@users.noreply.github.com>
2023-09-03 10:36:34 -0400

    Adds node debugger

---
+++ lua/benfiliatro/debugger.lua
@@ -0,0 +1,19 @@
+require("dap").adapters["pwa-node"] = {
+  type = "server",
+  host = "localhost",
+  port = "${port}",
+  executable = {
+    command = "node",
+    args = {"../../debugger/js-debug/src/dapDebugServer.js", "${port}"},
+  }
+}
+
+require("dap").configurations.javascript = {
+  {
+    type = "pwa-node",
+    request = "launch",
+    name = "Launch file",
+    program = "${file}",
+    cwd = "${workspaceFolder}",
+  },
+}
