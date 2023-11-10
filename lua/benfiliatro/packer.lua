4ffcb729b8cf4e98d713a67c9363c2c0b09b4aed
Benjamin Filiatrault <37634960+benjaminfiliatrault@users.noreply.github.com>
2023-11-10 17:14:00 -0500

    Improve packages

--- lua/benfiliatro/packer.lua
+++ lua/benfiliatro/packer.lua
@@ -1,4 +1,3 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -45,15 +44,31 @@ return require('packer').startup(function(use)
 		}
 	}
 
+    -- To easily surround selected word with quotes & other stuff
+    use({
+        "kylechui/nvim-surround",
+        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    })
 
-    use('godlygeek/tabular')
-    use('preservim/vim-markdown')
-    use('jose-elias-alvarez/null-ls.nvim')
-    use('MunifTanjim/prettier.nvim')
-    use('tpope/vim-surround')
+    -- Auto detect tab length
+    use('tpope/vim-sleuth')
+
+    -- Prettier recommended
     use('sbdchd/neoformat')
+    -- Eslint official 
     use('eslint/eslint')
+
+    use('preservim/vim-markdown')
+
+    -- For that pretty Status bar at the bottom
     use('vim-airline/vim-airline')
+
+    -- Improve comment writting
     use('terrortylor/nvim-comment')
 
     use {
