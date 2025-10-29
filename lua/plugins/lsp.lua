9b2038f051b1c728b8f23fd5ec8c330bd89d022a
Benjamin Filiatrault <37634960+benjaminfiliatrault@users.noreply.github.com>
2025-10-29 10:20:42 -0400

    improve tscontext

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,7 +2,6 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		"nvim-treesitter/nvim-treesitter-context",
 		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{
@@ -27,22 +26,6 @@ return {
 		},
 	},
 	config = function()
-		require("treesitter-context").setup({
-			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-			multiwindow = false, -- Enable multiwindow support.
-			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-			line_numbers = true,
-			multiline_threshold = 20, -- Maximum number of lines to show for a single context
-			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-			-- Separator between context and content. Should be a single character string, like '-'.
-			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-			separator = nil,
-			zindex = 20, -- The Z-index of the context window
-			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
-		})
-
 		-- import lspconfig plugin
 		local keymap = vim.keymap -- for conciseness
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,8 +4,31 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
 		"windwp/nvim-ts-autotag",
+		"nvim-treesitter/nvim-treesitter-context",
 	},
 	config = function()
+		local context = require("treesitter-context")
+		context.setup({
+			enable = false, -- Enable this plugin (Can be enabled/disabled later via commands)
+			multiwindow = false, -- Enable multiwindow support.
+			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+			line_numbers = true,
+			multiline_threshold = 20, -- Maximum number of lines to show for a single context
+			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+			-- Separator between context and content. Should be a single character string, like '-'.
+			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+			separator = nil,
+			zindex = 20, -- The Z-index of the context window
+			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+		})
+
+		local keymap = vim.keymap -- for conciseness
+		keymap.set("n", "tse", function()
+			context.toggle()
+		end)
+
 		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter.configs")
 
@@ -29,26 +52,27 @@ return {
 			autotag = { enable = true },
 			-- ensure these language parsers are installed
 			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"prisma",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
 				"bash",
-				"lua",
-				"vim",
+				"c",
+				"css",
 				"dockerfile",
 				"gitignore",
+				"graphql",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"prisma",
 				"query",
+				"svelte",
+				"tsx",
+				"typescript",
+				"rust",
+				"vim",
 				"vimdoc",
-				"c",
+				"yaml",
 			},
 			incremental_selection = {
 				enable = true,
